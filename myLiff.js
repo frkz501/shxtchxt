cccf1941ea87d6b93a3f1526f4e02d1303b03083
Patit Phatanarat <506566@pea.co.th>
2026-07-17 11:40:54 +0700

    integrating GAS registration

--- myLiff.js
+++ myLiff.js
@@ -5,11 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
             liff.login();
         } else {
             document.getElementById("loadingScreen").style.display = "none";
-            document.getElementById("registrationScreen").style.display = "block";
         }
     }).catch((err) => {
         document.getElementById("statusText").innerText = "LIFF Initialization Failed.";
         console.error(err);
+        return false;
+    });
+
+    // check if user is registered
+    liff.getProfile().then(profile => {
+        getUser(profile.userId).then(gotUser => {
+            if (!gotUser.registered) {
+                document.getElementById("registrationScreen").style.display = "block";
+            } else {
+                document.getElementById("dashboardScreen").style.display = "block";
+            }
+        })
     });
 
     // Handle form submission to send data back to your GAS Web App endpoint
@@ -18,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         liff.getProfile().then(profile => {
             const formData = {
                 userId: profile.userId,
+                dispName: profile.displayName,
                 fullName: document.getElementById("regFullName").value,
                 email: document.getElementById("regEmail").value,
                 employeeId: document.getElementById("regEmployeeId").value,
@@ -29,18 +41,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 dateOfHire: document.getElementById("regDateOfHire").value
             };
 
-            alert(JSON.stringify(formData));
-            // // Send this payload via fetch() to your Google Apps Script Web App URL (doPost)
-            // fetch("https://script.google.com/macros/s/AKfycby9VEvS-xMwieDqf57Udh1IJmLpoXdE2oNoowCdF1048Fi3qZnHx4f8z1TGPeW5Ps3kVg/exec", {
-            //     method: "POST",
-            //     body: JSON.stringify(formData)
-            // })
-            // .then(res => res.text())
-            // .then(result => {
-            //     alert("Registration successful!");
-            //     liff.closeWindow();
-            // })
-            // .catch(err => console.error("Error submitting form", err));
+            // Send this payload via fetch() to your Google Apps Script Web App URL (doPost)
+            fetch("https://script.google.com/macros/s/AKfycby9VEvS-xMwieDqf57Udh1IJmLpoXdE2oNoowCdF1048Fi3qZnHx4f8z1TGPeW5Ps3kVg/exec", {
+                method: "POST",
+                body: JSON.stringify(formData)
+            })
+            .then(res => res.json())
+            .then(data => {
+                alert(data.message)
+                if (data.status === "success") {
+                    document.getElementById("registrationScreen").style.display = "none";
+                    document.getElementById("welcomText").innerText = `Welcome, ${profile.displayName}!`
+                    document.getElementById("dashboardScreen").style.display = "block";
+                }
+            })
+            .then(result => {
+                alert("Registration successful!");
+                liff.closeWindow();
+            })
+            .catch(err => console.error("Error submitting form", err));
         });
     });
 });
+
+async function getUser(userId) {
+    fetch("https://script.google.com/macros/s/AKfycby9VEvS-xMwieDqf57Udh1IJmLpoXdE2oNoowCdF1048Fi3qZnHx4f8z1TGPeW5Ps3kVg/exec", {
+        method: "POST",
+        body: JSON.stringify({
+            userId: userId
+        })
+    })
+}
