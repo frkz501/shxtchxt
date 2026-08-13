19dd503223f3c63a10192e8c8389492c43572b5b
Patit Phatanarat <506566@pea.co.th>
2026-08-13 17:34:32 +0700

    check GAS data 5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3307782488" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "883050753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
